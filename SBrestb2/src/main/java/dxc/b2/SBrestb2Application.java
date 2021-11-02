5910bf335390597457ecac33b0ba222d5462c1c8
Jahnavi <Jahnavi@LAPTOP-LAKVACQC>
2021-11-02 12:02:35 +0530

    initial commit

---
+++ SBrestb2/src/main/java/dxc/b2/SBrestb2Application.java
@@ -0,0 +1,13 @@
+package dxc.b2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SBrestb2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SBrestb2Application.class, args);
+	}
+
+}
---
+++ SBrestb2/src/test/java/dxc/b2/SBrestb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package dxc.b2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SBrestb2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
